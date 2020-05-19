e4cf98d2bf0c76971956882d683b7de5e5e4c57d
Rafaello <rafjar@Rafalek>
2020-05-19 19:27:50 +0200

    Added comments to Message

--- src/server/Message.java
+++ src/server/Message.java
@@ -3,26 +3,26 @@ package server;
 public class Message 
 {
 	
-	private String msgContent;
+	private String msgContent; // Tekst wiadomości
 	private boolean msgFinal = false; // Jeśli to jest ostatnia wiadomość (połączenie zakończone), to true
 	
 	public Message(String msgContent, boolean msgFinal) 
 	{
-		this.msgContent = msgContent;
+		this.msgContent = msgContent; // Inicjalizacja zmiennych
 		this.msgFinal = msgFinal;
 	}
 	
 	public Message(String msgContent) 
 	{
-		this(msgContent, false);
+		this(msgContent, false); // Inicjalizacja zmiennych (zmienna msgFinal = false)
 	}
 	
-	public boolean finalMsg()
+	public boolean finalMsg() // Zwrócenie zmiennej msgFinal
 	{
 		return msgFinal;
 	}
 	
-	public String getContent()
+	public String getContent() // Zwrócenie tekstu wiadomości
 	{
 		return msgContent;
 	}
